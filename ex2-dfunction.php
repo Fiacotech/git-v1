b4e3ac07a7dd3ac0211687bf4a13822278f26e0f
Fiaco <fzonetech@gmail.com>
2023-08-14 12:58:09 +0100

    2nd commit

---
+++ ex2-dfunction.php
@@ -0,0 +1,11 @@
+<?php
+
+$i = [1, 2, 3];
+
+foreach ($i as $a){
+
+    echo "$a"."Second Modification for second commit" ;
+}
+
+
+?>
---
+++ exl-forloop.php
@@ -0,0 +1,13 @@
+<?php
+
+ $n = 1;
+
+
+ if($n=1){
+    echo "N is 1"."First Modification for First commit";
+ }
+
+
+
+
+?>
