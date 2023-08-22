599086214c92ae51de995f9b6dfad290530f6b67
Fiacotech <142477106+Fiacotech@users.noreply.github.com>
2023-08-22 14:01:00 +0100

    Updated exl-forloop.php from techteam1 branch on Github

--- exl-forloop.php
+++ exl-forloop.php
@@ -6,6 +6,7 @@
  if($n=1){
     echo "N is 1"."First Modification for First commit";
     echo "Added this line of code from Fiacotech, git terminal"
+     echo "Added 1st line from techteam1 branch of github";
  }
 
 
